a3daed1bdd689c9b78470c67ac3acc91b874b58f
Juan <juan.echeverry33010@ucaldas.edu.co>
2024-06-24 21:56:09 -0500

    eliminacion de codigo comentado usado como base

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -18,33 +18,7 @@ router.get("/todospanel", async (req, res) => {
     }
 });
 
-// router.get('/create', (req, res) => {
-//     res.render('create');
-//    });
-// // Store
-//  router.post("/", async (req, res) => {
-//      try {
-//          const {title, completed} = req.body;
-//          await Todo.create({ title, completed: completed == 'on' ? true : false });
-//          res.redirect('/todospanel');
-//      } catch (error) {
-//          res.status(500).send(error.message);
-//      }
-//  });
 
-
-
-// // Obtener todas 
-// router.get('/', async (req, res) => {
-//     try {
-//       const todos = await todoModel.findAll();
-//       res.render('todos/index', { todos, search: req.query.search || '' });
-//     } catch (error) {
-//       console.error('Error al obtener tarea:', error);
-//       res.status(500).json({ error: 'Error al obtener tarea' });
-//     }
-//   });
-  
 //   // Obtener una tarea por ID
   router.post('/todos', async (req, res) => {
     const body = req.body;
@@ -80,39 +54,7 @@ router.get("/todospanel", async (req, res) => {
       }
     });
   
-//   // Actualizar una tarea por ID (formulario)
-//   router.get('/todos/:id/edit', async (req, res) => {
-//     const todoId = req.params.id;
-//     try {
-//       const todo = await todoModel.findByPk(todoId);
-//       if (todo) {
-//         res.render('todos/edit', { todo });
-//       } else {
-//         res.status(404).json({ error: 'tarea no encontrado' });
-//       }
-//     } catch (error) {
-//       console.error('Error al obtener tarea por ID:', error);
-//       res.status(500).json({ error: 'Error al obtener tarea por ID' });
-//     }
-//   });
-  
-//   // Procesar la actualización de la tarea
-//   router.put('/todos/:id', async (req, res) => {
-//     const todoId = req.params.id;
-//     const datosActualizados = req.body;
-//     try {
-//       const todo = await todoModel.findByPk(todoId);
-//       if (todo) {
-//         await todo.update(datosActualizados);
-//         res.redirect(`/todos/${todoId}`);
-//       } else {
-//         res.status(404).json({ error: 'tarea no encontrada' });
-//       }
-//     } catch (error) {
-//       console.error('Error al actualizar deporte por ID:', error);
-//       res.status(500).json({ error: 'Error al actualizar tarea por ID' });
-//     }
-//   });
+
   
 //   // Eliminar una tarea por ID
   router.post('/todos/delete/:id', async (req, res) => {
